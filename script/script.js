f4b42fe6de02451fa4e7adb80b5743f50306f174
anshul singh <singhanshul2807@gmail.com>
2021-12-28 00:00:27 +0530

    added Slider LocalStorage

--- script/script.js
+++ script/script.js
@@ -6,32 +6,48 @@ let darksvg = document.querySelector(".navbar__darkmode")
 
 let lists = document.querySelector('.navbar__lists li');
 
+// local var
+let html,css,js;
+let key='WEBDEVEDITOR';
+
+
+
+
 
  const showPreview = () => {
-    var html = htmlCode.value;
-    var css = "<style>"+cssCode.value+"</style>";
-    var js = "<scri"+"pt>"+jsCode.value+"</scri"+"pt>";
+     html = htmlCode.value;
+     css = "<style>"+cssCode.value+"</style>";
+     js = "<script>"+jsCode.value+"</script>";
+
+   
+
     var frame = iframe.contentWindow.document;
     
     frame.open();
     frame.write(html+css+js);
     frame.close();
+
+    localStorage.setItem(`${key}HTML`,html);
+    localStorage.setItem(`${key}CSS`,cssCode.value);
+    localStorage.setItem(`${key}JS`,jsCode.value);
+
   }
-  const clearSection = (element) =>{
-    
-     if(element == 'html')
-     {
-        htmlCode.value = '';
-     }
-     if(element == 'css')
-     {
-        cssCode.value = '';
-     }
-     if(element == 'js')
-     {
-        jsCode.value = '';
-     }
+  const clearSection = (key) =>{
+     switch (key) {
+        case 'html':
+           htmlCode.value = '';
+           break;
+        case 'css':
+           cssCode.value = '';
+           break;
+        case 'js':
+           jsCode.value = '';
+           break;
+                
      
+        default:
+           break;
+     }     
      showPreview()
   }
 
@@ -65,3 +81,47 @@ let lists = document.querySelector('.navbar__lists li');
    darksvg.style.marginBottom = "-2px";
    
   }
+
+  // drag cursor
+
+  const resizer = document.getElementById('dragable');
+  let code_area = document.querySelector('.code-area');
+
+  resizer.addEventListener("mousedown", (event) => {
+   document.addEventListener("mousemove", resize, false);
+   document.addEventListener("mouseup", () => {
+     document.removeEventListener("mousemove", resize, false);
+   }, false);
+ });
+ 
+ function resize(e) {
+   const size = `${e.x}px`;
+   code_area.style.width = size;
+ }
+ window.onload=()=>{
+    var L_html,L_css,L_js;
+    if(localStorage.getItem(`${key}HTML`)){
+      L_html = localStorage.getItem(`${key}HTML`);
+      htmlCode.innerHTML = L_html
+    }
+
+    if(localStorage.getItem(`${key}CSS`)){
+       L_css=localStorage.getItem(`${key}CSS`);
+      cssCode.innerHTML = L_css;
+    }
+
+    if(localStorage.getItem(`${key}JS`)){
+       L_js = localStorage.getItem(`${key}JS`);
+      jsCode.innerHTML = L_js;
+    }
+
+    L_html = L_html;
+    L_css = "<style>"+L_css+"</style>";
+    L_js = "<script>"+L_js+"</script>";
+
+    var frame = iframe.contentWindow.document;
+    
+    frame.open();
+    frame.write(L_html+L_css+L_js);
+    frame.close();
+ }
